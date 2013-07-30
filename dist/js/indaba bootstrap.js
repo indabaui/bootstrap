6c0e5a875683f137b739df1f190396d41ff05159
Ethan Fuchs <ethan@indabamusic.com>
2013-07-30 14:25:31 -0400

    updated package, updated dist

--- dist/js/indaba bootstrap.js
+++ dist/js/indaba bootstrap.js
@@ -1,7 +1,7 @@
 /**
-* bootstrap.js v3.0.0 by @fat and @mdo
-* Copyright 2013 Twitter Inc.
-* http://www.apache.org/licenses/LICENSE-2.0
+* indaba bootstrap.js v3.0.0 by @fat and @mdo
+* Copyright 2013 Indaba via Twitter Inc.
+* 
 */
 if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
 
